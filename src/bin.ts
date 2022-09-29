88b0c5da0c19f26c0e8da9df34f5c6dd54a71b32
Jeppe Reinhold <jeppe@chromatic.com>
2022-09-30 01:44:43 +0200

    Add auto for releasing and CI (#2)

--- src/bin.ts
+++ src/bin.ts
@@ -9,11 +9,11 @@ const main = async () => {
     .epilog('For more information, see https://github.com/storybookjs/create-svelte-with-args')
     .example([
       [
-        'npx $0 --name=my-new-app --template=default --types=checkjs --prettier --no-eslint --no-playwright',
+        'npm create svelte-with-args@latest --name=my-new-app --template=default --types=checkjs --prettier --no-eslint --no-playwright',
         'Default app, checking JS types, with Prettier, and no ESLint or Playwright',
       ],
       [
-        'npx $0 --name=my-new-app --directory=other-dir/my-app --template=default --types=typescript --no-prettier --no-eslint --no-playwright',
+        'npm create svelte-with-args@latest --name=my-new-app --directory=other-dir/my-app --template=default --types=typescript --no-prettier --no-eslint --no-playwright',
         'Default app, installing in specified directory',
       ],
     ])
